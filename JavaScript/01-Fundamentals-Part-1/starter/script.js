9e73c40bcc64f20867d8c1d1deb3c8b59ceb6f04
PhilipSanM <felipesanchezmart@gmail.com>
2022-03-30 08:18:53 -0600

    Learning variables and values

--- JavaScript/01-Fundamentals-Part-1/starter/script.js
+++ JavaScript/01-Fundamentals-Part-1/starter/script.js
@@ -1,13 +1,97 @@
 
-// let is for making variables
-let js = "amazing";
 
-if (js=="amazing")
-    alert("Javascript is FUN!");
-//Values are the smallest unit of information on javascript
-console.log(40+8+23-10);
+// //=======================================================
+// //=========== V A  L U E S & V A R I A B L E S ==========
+// //=======================================================
+
+// //Variable name convention
+// // let is for making variables
+// let js = "amazing";
+
+// if (js=="amazing")
+//     alert("Javascript is FUN!");
+// //Values are the smallest unit of information on javascript
+// console.log(40+8+23-10);
+// //console.log is used to show something in the console html
 
 
-//We make a variable Firstname with the name "Jonas"
-let firstName = "Jonas";
-console.log(firstName);
+// //We make a variable Firstname with the name "Jonas"
+// let firstName = "Jonas";
+// let secondName= "Santiago";
+// //So we can print it on the console
+// //To use the console, just click on developer tools on the navegator
+// console.log(firstName);
+
+// /*
+//     <>camelCase:(if u have more than one word) write the first word in lower and the second in upper
+//     <>You can't use a special keyword of javascript
+//     <>you can use letter, dollar sign, and number but not at the begin of the variable
+//     <>You can't use an uppercase letter at the begining (because the convention)
+//     <> constant will be written in all uppercase
+//     <> the value should be easy to understand by reading the variable
+
+// */
+
+// // My personal information ASSIGNEMENT:
+// let country = "Mexico";
+// let continent = "America";
+// let population = "45M";
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+
+// //=======================================================
+// //=========== D A T A  T Y P E S ========================
+// //=======================================================
+// //JvS is executed from the top to the bottom
+// //NUMBER
+// let age=18;
+// //String, in qoutes, with one or two are the same
+// let firstNameA = 'Felix';
+// //BOOL
+// let fullAge = true;
+// console.log(false);
+
+// //typeof - show the type of the variable
+// console.log(typeof fullAge);
+
+
+// //When you want to change the value of a variable
+// //first declare thevariable and then declare it without let
+// console.log(age);
+// age = 'GG!';
+// console.log(age);
+
+// //there exists also undefined variables if you just:
+// let undefinedVariable;
+// console.log(undefinedVariable);
+// console.log(typeof undefinedVariable);
+
+// undefinedVariable=1999;
+// console.log(typeof undefinedVariable);
+
+// // =======================================================
+// // =========== D E C L A R E D  V A R I A B L E S ========
+// // =======================================================
+
+// //You can delcared with let, and also empty variables
+// let age =30;
+// age =21;
+
+// //variables that can not be change with constant, and can't also be empty variables
+// const birthYear=2003;
+// //prior to ES6 we use var instead of let
+
+// var job='student';
+// job='IBMengineer';
+
+// //You can also declared a variable without let, var or constant
+// globalVariable=34;
+// console.log(globalVariable);
+// //it works, but isnot a variable that you can use
+
+// =======================================================
+// =========== B A S I C  O P E R A T O R S ==============
+// =======================================================
+
