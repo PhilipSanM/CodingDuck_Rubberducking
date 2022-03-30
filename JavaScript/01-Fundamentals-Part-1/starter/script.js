00268922a09f160b1f9fb6c7b7542b59987a2be0
PhilipSanM <felipesanchezmart@gmail.com>
2022-03-29 22:40:59 -0600

    fundamental code of javascript

---
+++ JavaScript/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,13 @@
+
+// let is for making variables
+let js = "amazing";
+
+if (js=="amazing")
+    alert("Javascript is FUN!");
+//Values are the smallest unit of information on javascript
+console.log(40+8+23-10);
+
+
+//We make a variable Firstname with the name "Jonas"
+let firstName = "Jonas";
+console.log(firstName);
