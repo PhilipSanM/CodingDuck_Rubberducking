85238bfc8a9a24f001b38f1fbb31de023888250d
PhilipSanM <felipesanchezmart@gmail.com>
2022-06-19 20:13:30 -0500

    Switch and challenge#3

--- JavaScript/01-Fundamentals-Part-1/starter/script.js
+++ JavaScript/01-Fundamentals-Part-1/starter/script.js
@@ -344,7 +344,97 @@
 // 	console.log('This line will not work');
 // }
 
-const hasDriversLicence = true; //A
-const hasGoodVision = true;//B
+// const hasDriversLicence = true; //A
+// const hasGoodVision = true;//B
 
-console.log(hasDriversLicence && hasGoodVision);
+// console.log(hasDriversLicence && hasGoodVision);
+
+// console.log(hasDriversLicense && hasGoodVision);
+// console.log(hasDriversLicense || hasGoodVision);
+// console.log(!hasDriversLicense);
+
+// // if (hasDriversLicense && hasGoodVision) {
+// //   console.log('Sarah is able to drive!');
+// // } else {
+// //   console.log('Someone else should drive...');
+// // }
+
+// const isTired = false; // C
+// console.log(hasDriversLicense && hasGoodVision && isTired);
+
+// if (hasDriversLicense && hasGoodVision && !isTired) {
+//   console.log('Sarah is able to drive!');
+// } else {
+//   console.log('Someone else should drive...');
+// }
+
+
+// // // =======================================================
+// // // =========== CODING CHALLENGE #3 =======================
+// // // =======================================================
+
+
+// There are two gymnastics teams, Dolphins and Koalas. They compete against each other 3 times. The winner with the highest average score wins the a trophy!
+
+// 1. Calculate the average score for each team, using the test data below
+// 2. Compare the team's average scores to determine the winner of the competition, and print it to the console. Don't forget that there can be a draw, so test for that as well (draw means they have the same average score).
+
+// 3. BONUS 1: Include a requirement for a minimum score of 100. With this rule, a team only wins if it has a higher score than the other team, and the same time a score of at least 100 points. HINT: Use a logical operator to test for minimum score, as well as multiple else-if blocks 😉
+// 4. BONUS 2: Minimum score also applies to a draw! So a draw only happens when both teams have the same score and both have a score greater or equal 100 points. Otherwise, no team wins the trophy.
+
+// TEST DATA: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+// TEST DATA BONUS 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+// TEST DATA BONUS 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+
+// GOOD LUCK 😀
+
+
+// // const scoreDolphins = (96 + 108 + 89) / 3;
+// // const scoreKoalas = (88 + 91 + 110) / 3;
+
+// // if (scoreKoalas > scoreDolphins) {
+// // 	console.log('Team Koalas win!!');
+// // } else if (scoreKoalas < scoreDolphins) {
+// // 	console.log('Team Dolphins win!!');
+// // }else{
+// // 	console.log('There is a draw');
+// // }
+
+// //BONUS 1
+// const scoreDolphins = (97 + 112 + 101) / 3;
+// const scoreKoalas = (109 + 95 + 123) / 3;
+
+// if (scoreKoalas > scoreDolphins && scoreKoalas >= 100) {
+// 	console.log('Team Koalas win!!');
+// } else if (scoreKoalas < scoreDolphins && scoreDolphins >= 100) {
+// 	console.log('Team Dolphins win!!');
+// }else if (scoreKoalas === scoreDolphins && scoreDolphins >= 100 && scoreKoalas >= 100) {
+// 	console.log('There is a draw')
+// }else{
+// 	console.log("No one wins");
+// }
+
+
+// =======================================================
+// =========== SWITCH STATMENT ===========================
+// =======================================================
+
+const day = 'monday';
+switch(day) {
+	case 'monday': // day === 'monday'
+		console.log('Plan course strcuture');
+		console.log('Go fot it');
+		break;
+	case 'tuesday':
+		console.log('make my video');
+		break;
+	case 'thursday':
+		console.log('IDK');
+		break;
+	case 'friday':
+		console.log('lol');
+		break;
+	default:
+		console.log("error that's not a day");	
+		break;
+}
