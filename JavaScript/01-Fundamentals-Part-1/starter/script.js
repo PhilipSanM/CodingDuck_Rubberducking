de320f4df864d960ee558257fffdabbdec7e9f38
PhilipSanM <felipesanchezmart@gmail.com>
2022-06-19 20:39:28 -0500

    challenge#4

--- JavaScript/01-Fundamentals-Part-1/starter/script.js
+++ JavaScript/01-Fundamentals-Part-1/starter/script.js
@@ -482,25 +482,47 @@
 // // =======================================================
 // // =========== TERNARY CONDITIONAL ========================
 // // =======================================================
-const age = 18;
+// const age = 18;
 
-age >= 18 ? console.log('I wanna drink my beer'):
-console.log('Just watter little boy');
+// age >= 18 ? console.log('I wanna drink my beer'):
+// console.log('Just watter little boy');
 
-//CONDITION, IF part, and else part
+// //CONDITION, IF part, and else part
 
-//A VALUE NEEDS AN EXPRESION SO U CAN DO THIS
+// //A VALUE NEEDS AN EXPRESION SO U CAN DO THIS
 
-const drink = age >= 18 ? 'wine': 'water';
-console.log(drink);
+// const drink = age >= 18 ? 'wine': 'water';
+// console.log(drink);
 
-let drink2;
-if (age >= 18) {
-  drink2 = 'wine 🍷';
-} else {
-  drink2 = 'water 💧';
-}
-console.log(drink2);
-//OR u can put that, because is an expresion, or u can make it a value
-console.log(`I like to drink ${age >= 18 ? 'wine 🍷' : 'water 💧'}`);
-console.log(`I like to drink ${drink}`);
+// let drink2;
+// if (age >= 18) {
+//   drink2 = 'wine 🍷';
+// } else {
+//   drink2 = 'water 💧';
+// }
+// console.log(drink2);
+// //OR u can put that, because is an expresion, or u can make it a value
+// console.log(`I like to drink ${age >= 18 ? 'wine 🍷' : 'water 💧'}`);
+// console.log(`I like to drink ${drink}`);
+// // // =======================================================
+// // // =========== CODING CHALLENGE #4 =======================
+// // // =======================================================
+
+
+// Steven wants to build a very simple tip calculator for whenever he goes eating in a resturant. In his country, it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
+
+// 1. Your task is to caluclate the tip, depending on the bill value. Create a variable called 'tip' for this. It's not allowed to use an if/else statement 😅 (If it's easier for you, you can start with an if/else statement, and then try to convert it to a ternary operator!)
+// 2. Print a string to the console containing the bill value, the tip, and the final value (bill + tip). Example: 'The bill was 275, the tip was 41.25, and the total value 316.25'
+
+// TEST DATA: Test for bill values 275, 40 and 430
+
+// HINT: To calculate 20% of a value, simply multiply it by 20/100 = 0.2
+// HINT: Value X is between 50 and 300, if it's >= 50 && <= 300 😉
+
+// GOOD LUCK 😀
+
+
+// const bill = 275 ;
+// const tip = bill >= 50 && bill <= 300 ? bill*0.15: bill*0.2;
+
+// console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`)
