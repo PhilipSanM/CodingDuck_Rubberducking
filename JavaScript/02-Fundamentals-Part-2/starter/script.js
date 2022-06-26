6f15408926a42de3305ca8ed3b17a3c4330532be
PhilipSanM <felipesanchezmart@gmail.com>
2022-06-26 11:40:46 -0500

    Strict mode

---
+++ JavaScript/02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,14 @@
+'use strict';
+//STRICT MODE this is gonna be the first line
+//Instroduce bugs and visible errors
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive');
+
+//If you have an error, strictmode shows you where is and what happend
+
+// const interface = 'Audio';
+let const = 24;
