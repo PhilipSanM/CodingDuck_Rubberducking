5317daa3a7d2652339a48ec861f83e433ba9452f
PhilipSanM <felipesanchezmart@gmail.com>
2022-07-09 18:09:29 -0500

    JS/Starting with React

--- JavaScript/09-Data-Structures-Operators/starter/script.js
+++ JavaScript/09-Data-Structures-Operators/starter/script.js
@@ -12,6 +12,10 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -26,4 +30,114 @@ const restaurant = {
       close: 24,
     },
   },
+  // A function that recives an object with default values
+  orderDelivery: function ({
+    starterIndex = 0,
+    adress = 'No one',
+    time = '24:00',
+  }) {
+    console.log(
+      `Order Received! ${this.starterMenu[starterIndex]} will be delivered to ${adress} at ${time}`
+    );
+  },
 };
+
+restaurant.orderDelivery({
+  time: '23:00',
+  adress: 'San Lucas',
+  mainIndex: 2,
+  starterIndex: 4,
+});
+
+// //======================================================
+// //========= D E S T R U C T U R I N G  A R R A Y S =====
+// //======================================================
+//
+// //There are some ways to copy info from an array like
+// const array = [2, 43, 56];
+// const a = array[0];
+// const b = array[1];
+// const c = array[2];
+
+// //OR like
+// const [x, y, z] = array;
+// console.log(x, y, z);
+// console.log(array);
+// //tHATS Destructuring the array
+
+// //NOW LETS TALK ABOUT THE OBJECT
+// let [first, second] = restaurant.categories;
+// console.log(first, second);
+
+// //For a third element :
+
+// const [primary, , third] = restaurant.categories;
+// console.log(primary, third);
+
+// //Making a swith with two variables
+// //You should use a temporary
+// // TIRCK WITH Destructuring
+// [first, second] = [second, first];
+// console.log(first, second);
+
+// console.log(restaurant.order(2, 0));
+// const [sarter, main] = restaurant.order(2, 0);
+// console.log(sarter, main);
+
+// //Array inside an array:
+
+// const nested = [2, 5, [2, 5]];
+// const [i, , j] = nested;
+// console.log(i, j);
+// //Destructuring an array with an array xd
+
+// const [q, w, [r, t]] = nested;
+// console.log(q, w, r, t);
+
+// //Also you can read the elemente of an array with
+// const [f, n, m] = [4, 6];
+// console.log(f, n, m);
+
+// //======================================================
+// //========= D E S T R U C T U R I N G  O B J E C T S ===
+// //======================================================
+// NOTE: BUG FIXME OK
+//{} for objects an [] for aarays
+//But you have to use the name of the object
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+//if you want to change the name is with :
+const { name: nameres, openingHours: hours, categories: pizzas } = restaurant;
+console.log(nameres, hours, pizzas);
+
+//Or if you work with default values lije
+// const { menu = [], starterMenu: menuS = [] } = restaurant; //This give you undefined for menu
+// console.log(menu, menuS);
+
+const { menu = [], starterMenu: menuS = [] } = restaurant; //This give you [] for menu
+console.log(menu, menuS);
+
+//NOTE: For overwrite a variable is betwen () because:
+
+let a = 12;
+let b = 343;
+const obj = {
+  a: 23,
+  b: 2,
+  c: 4,
+};
+({ a, b } = obj);
+console.log(a, b);
+
+//An object inside an object so ...
+//Nested objects
+
+const {
+  fri: { open, close },
+} = openingHours;
+console.log(open, close);
+
+const {
+  fri: { open: z, close: x },
+} = openingHours;
+console.log(z, x);
