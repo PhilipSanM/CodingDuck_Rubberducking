43a2cd542ed4fd2b221888c70d7fd4d25af8dae1
PhilipSanM <felipesanchezmart@gmail.com>
2022-07-22 11:56:26 -0500

    Challenge #4

--- JavaScript/09-Data-Structures-Operators/starter/script.js
+++ JavaScript/09-Data-Structures-Operators/starter/script.js
@@ -1081,30 +1081,46 @@ Afterwards, test with your own test data!
 
 GOOD LUCK 😀
 */
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
 
-// // //======================================================
-// // //========= PROBLEM ====================================
-// // //======================================================
+document.querySelector('button').addEventListener('click', function () {
+  const info = document.querySelector('textarea').value;
+  let i = 0;
+  for (const data of info.split('\n')) {
+    const dataLower = data.trim().toLowerCase();
+    const [first, second] = dataLower.split('_');
 
-// 🔴 Delayed Departure from FAO to TXL (11h25)
-//              Arrival from BRU to FAO (11h45)
-//   🔴 Delayed Arrival from HEL to FAO (12h05)
-//            Departure from FAO to LIS (12h30)
-// const flights =
-// '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+    const output = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+    console.log(`${output.padEnd(20)}${'✅'.repeat(++i)}`);
+  }
+});
+// // // //======================================================
+// // // //========= PROBLEM ====================================
+// // // //======================================================
 
-console.log(flights.split('+'));
-const strFlights = flights.split('+');
-for (const fly of strFlights) {
-  const [type, from, to, time] = fly.split(';');
-  console.log(
-    `${type.includes('Delayed') ? '🔴' : ''}${type.replaceAll(
-      '_',
-      ' '
-    )} from ${from.slice(0, 3).toUpperCase()} to ${to
-      .slice(0, 3)
-      .toUpperCase()} (${time.replace(':', 'h')})
+// // 🔴 Delayed Departure from FAO to TXL (11h25)
+// //              Arrival from BRU to FAO (11h45)
+// //   🔴 Delayed Arrival from HEL to FAO (12h05)
+// //            Departure from FAO to LIS (12h30)
+// // const flights =
+// // '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
-  `.padStart(36)
-  );
-}
+// console.log(flights.split('+'));
+// const strFlights = flights.split('+');
+// for (const fly of strFlights) {
+//   const [type, from, to, time] = fly.split(';');
+//   console.log(
+//     `${type.includes('Delayed') ? '🔴' : ''}${type.replaceAll(
+//       '_',
+//       ' '
+//     )} from ${from.slice(0, 3).toUpperCase()} to ${to
+//       .slice(0, 3)
+//       .toUpperCase()} (${time.replace(':', 'h')})
+
+//   `.padStart(36)
+//   );
+// }
