35577d71aa5bb1a95caf262b110ca16d59346806
PhilipSanM <felipesanchezmart@gmail.com>
2022-07-26 12:08:47 -0500

    Gatsby build

---
+++ Gatsby/tutorial/src/pages/company/history.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Link } from "gatsby"
+const history = () => {
+  return (
+    <div>
+      <h2>this is the history of our company</h2>
+      <Link to="/">Home</Link>
+    </div>
+  )
+}
+
+export default history
--- Gatsby/tutorial/src/pages/index.js
+++ Gatsby/tutorial/src/pages/index.js
@@ -1,5 +1,16 @@
 import React from "react"
-
+import { Link } from "gatsby"
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <h1>Hello world! :)</h1>
+      <Link to="/about"> About </Link>
+      <div>
+        <a href="/about">reg Link</a>
+      </div>
+      <div>
+        <Link to="/company/history">history</Link>
+      </div>
+    </div>
+  )
 }
