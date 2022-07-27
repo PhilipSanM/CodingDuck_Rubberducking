2c5018af81837c8034cdbf3f79391e9c3e1f7dff
PhilipSanM <felipesanchezmart@gmail.com>
2022-07-27 18:49:56 -0500

    IIFE and code challenge

--- JavaScript/10-Functions/starter/script.js
+++ JavaScript/10-Functions/starter/script.js
@@ -113,9 +113,9 @@
 
 // //ABSTRACTION
 
-// //==================================================================
-// //=============== FUNCTIONS RETURNING FUNCTIONS ====================
-// //==================================================================
+// // //==================================================================
+// // //=============== FUNCTIONS RETURNING FUNCTIONS ====================
+// // //==================================================================
 
 // //Closure
 // const greet = function (greeting) {
@@ -139,3 +139,242 @@
 // const greetArrow = greeting => name => console.log(`${greeting} ${name}`);
 
 // greetArrow('Helo')('Philip');
+
+// // //==================================================================
+// // //=============== CALL AND APPLY METHODS ====================
+// // //==================================================================
+
+// const lufthansa = {
+//   airline: 'Lufthansa',
+//   iataCode: 'LH',
+//   bookings: [],
+//   // book: functuin(){}
+//   book(flightNum, name) {
+//     console.log(
+//       `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+//     );
+//     this.bookings.push({
+//       flight: `${this.iataCode}${flightNum}`,
+//       name: `${name}`,
+//       flightNum,
+//     });
+//   },
+// };
+
+// lufthansa.book(238, 'Philip');
+// lufthansa.book(245, 'Johan');
+// const eurowings = {
+//   airline: 'Eurowings',
+//   iataCode: 'EW',
+//   bookings: [],
+// };
+// const book = lufthansa.book;
+
+// //NOTE: THIS LINE DOESN'T WORK:
+// // book(23, 'Felix');
+
+// //CALL METHOD
+// book.call(eurowings, 23, 'Felix');
+// console.log(eurowings);
+
+// //we call the call method that calls the book function in eurowings lihe 'this'
+// book.call(lufthansa, 55, 'FelixLuft'); // pointnig to lufthansa as 'this'
+// console.log(lufthansa);
+
+// const swiss = {
+//   airline: 'Swiss Air Lines',
+//   iataCode: 'LX',
+//   bookings: [],
+// };
+// //You call and insert the data
+// book.call(swiss, 565, 'Mari curie');
+
+// //APPLY METHOD
+
+// //the same but this take an array of arguments not values
+// //Is better to use call
+// const flightData = [898, 'Georgio loreno'];
+
+// book.apply(swiss, flightData);
+// book.call(lufthansa, ...flightData);
+
+// //Bind Method
+// //Returns a new function, with the new keyword ej:
+// const bookEW = book.bind(eurowings);
+
+// bookEW(56, 'Steven');
+// const bookLH = book.bind(lufthansa);
+// const bookLX = book.bind(swiss);
+// //For defaul parametters like
+// const bookEWD = book.bind(eurowings, 23);
+// //So it only needs the name
+// bookEWD('Sheldoon copo');
+
+// //Objects together with eventListeners
+// lufthansa.planes = 30;
+// lufthansa.buyPlane = function () {
+//   //this is calling the event listener
+//   console.log(this);
+//   this.planes++;
+//   console.log(this.planes);
+// };
+// document
+//   .querySelector('.buy')
+//   .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+// //Because bind returns a function and we need a function, but before that this
+// // points to the eventLisstener, but now it points to the
+// // object
+
+// const addTax = (rate, value) => value + value * rate;
+// console.log(addTax(0.1, 200));
+
+// //You need the tax because we want to know if that is 23%, but the first parameter is this, but you dont care
+
+// const addVAT = addTax.bind(null, 0.23);
+// //The orden of the arguments is important xd
+// console.log(addVAT(23));
+
+// //Thats the same as:
+// const addTaxes = function (rate) {
+//   return function (value) {
+//     return value + value * rate;
+//   };
+// };
+
+// const taxVat = addTaxes(0.23);
+// console.log(taxVat(23));
+
+// // // //==================================================================
+// // // //===================== CODING CHALLENGE #1 ========================
+// // // //==================================================================
+
+// /*
+// Let's build a simple poll app!
+
+// A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter object below.
+
+// Here are your tasks:
+
+// 1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
+//   1.1. Display a prompt window for the user to input the number of the selected option. The prompt should look like this:
+//         What is your favourite programming language?
+//         0: JavaScript
+//         1: Python
+//         2: Rust
+//         3: C++
+//         (Write option number)
+
+//   1.2. Based on the input number, update the answers array. For example, if the option is 3, increase the value AT POSITION 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?)
+// 2. Call this method whenever the user clicks the "Answer poll" button.
+// 3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1".
+// 4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
+
+// HINT: Use many of the tools you learned about in this and the last section 😉
+
+// BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do NOT put the arrays in the poll object! So what shoud the this keyword look like in this situation?
+
+// BONUS TEST DATA 1: [5, 2, 3]
+// BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
+
+// GOOD LUCK 😀
+// */
+
+// // const poll = {
+// //   question: 'What is your favourite programming language?',
+// //   options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+// //   // This generates [0, 0, 0, 0]. More in the next section 😃
+// //   answers: new Array(4).fill(0),
+// //   registerNewAnswer: function () {
+// //     const answerUser = Number(
+// //       prompt(
+// //         `${this.question}\n${this.options.join('\n')}\n (Write option number)`
+// //       )
+// //     );
+// //     answerUser >= 0 && answerUser < 4
+// //       ? this.answers[answerUser]++
+// //       : console.log('Thats not a correct value try again');
+// //     this.displayResults(this.answers);
+// //   },
+// //   displayResults: function (type) {
+// //     console.log(`Poll results are: ${type}`);
+// //   },
+// // };
+
+// // document
+// //   .querySelector('.poll')
+// //   .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+// // poll.displayResults([5, 2, 3]);
+// // poll.displayResults([1, 5, 3, 9, 6, 1]);
+// const poll = {
+//   question: 'What is your favourite programming language?',
+//   options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+//   // This generates [0, 0, 0, 0]. More in the next section 😃
+//   answers: new Array(4).fill(0),
+//   registerNewAnswer() {
+//     // Get answer
+//     const answer = Number(
+//       prompt(
+//         `${this.question}\n${this.options.join('\n')}\n(Write option number)`
+//       )
+//     );
+//     console.log(answer);
+
+//     // Register answer
+//     typeof answer === 'number' &&
+//       answer < this.answers.length &&
+//       this.answers[answer]++;
+
+//     this.displayResults();
+//     this.displayResults('string');
+//   },
+
+//   displayResults(type = 'array') {
+//     if (type === 'array') {
+//       console.log(this.answers);
+//     } else if (type === 'string') {
+//       // Poll results are 13, 2, 4, 1
+//       console.log(`Poll results are ${this.answers.join(', ')}`);
+//     }
+//   },
+// };
+
+// document
+//   .querySelector('.poll')
+//   .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+// poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
+// poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
+// poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
+
+// // [5, 2, 3]
+// // [1, 5, 3, 9, 6, 1]
+
+// //==================================================================
+// //======= INMEDIATELY Invoked function EXPRESSIONS========================
+// //==================================================================
+// IIFE
+
+//A function that desappeared once it is called
+
+(function () {
+  console.log('This function will be deleted');
+})();
+// You can call it when you put the () in the function to think JS that is an expresion
+//and you call it with the () at the end
+
+//Arrow IIFE
+(() => console.log('This function will be deleted'))();
+
+{
+  const isPrivate = 23;
+  var notPrivate = 46;
+}
+// console.log(isPrivate);
+// console.log(notPrivate);
+
+//Is like a block
+
+// //==================================================================
+// //======= CLOSURES ========================
+// //==================================================================
