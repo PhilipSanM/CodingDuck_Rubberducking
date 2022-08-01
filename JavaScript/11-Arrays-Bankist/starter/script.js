b5478521bed8149979b2e03db41ddfc44088712c
PhilipSanM <felipesanchezmart@gmail.com>
2022-08-01 08:44:56 -0500

    Coding challenge #1 array methods

--- JavaScript/11-Arrays-Bankist/starter/script.js
+++ JavaScript/11-Arrays-Bankist/starter/script.js
@@ -127,9 +127,9 @@ const inputClosePin = document.querySelector('.form__input--pin');
 // console.log(letters.join('-')); // a-b-c-d-e-f-j-g-h-i-j
 // console.log(letters.join(' - ')); // a - b - c - d - e - f - j - g - h - i - j
 
-// // ==================================================================
-// // ====================== AT METHOD ES6======================
-// // ==================================================================
+// // // ==================================================================
+// // // ====================== AT METHOD ES6======================
+// // // ==================================================================
 
 // const arr = [23, 11, 64];
 // //If we want to take one element fo the array we usually...
@@ -238,32 +238,67 @@ const inputClosePin = document.querySelector('.form__input--pin');
 // });
 // //We can add a _ because is innecesari
 
-// // ==================================================================
-// // =================== BANKIST APP ==================================
-// // ==================================================================
+// // // ==================================================================
+// // // =================== BANKIST APP ==================================
+// // // ==================================================================
 
-//Display all the movments
-const displayMovments = function (movements) {
-  // we can clean that part of the html with
-  // inner gives you the html of that class, but that will be clean ''
-  containerMovements.innerHTML = '';
-  //.textContent = 0;
-  movements.forEach(function (mov, i) {
-    const type = mov > 0 ? 'deposit' : 'withdrawal';
-    //Thats just creating an html array with that mehtod
-    const html = `
-    <div class="movements__row">
-      <div class="movements__type movements__type--${type}">${
-      i + 1
-    } ${type}</div>
-      <div class="movements__value">${mov}</div>
-    </div>
-    `;
-    // Now that we make the html we will insert the html with a method
-    containerMovements.insertAdjacentHTML('afterbegin', html);
-    // The fisrt parametter is the position wehre to atach
-    // All the variables are in the top of the JS
+// //Display all the movments
+// const displayMovments = function (movements) {
+//   // we can clean that part of the html with
+//   // inner gives you the html of that class, but that will be clean ''
+//   containerMovements.innerHTML = '';
+//   //.textContent = 0;
+//   movements.forEach(function (mov, i) {
+//     const type = mov > 0 ? 'deposit' : 'withdrawal';
+//     //Thats just creating an html array with that mehtod
+//     const html = `
+//     <div class="movements__row">
+//       <div class="movements__type movements__type--${type}">${
+//       i + 1
+//     } ${type}</div>
+//       <div class="movements__value">${mov}</div>
+//     </div>
+//     `;
+//     // Now that we make the html we will insert the html with a method
+//     containerMovements.insertAdjacentHTML('afterbegin', html);
+//     // The fisrt parametter is the position wehre to atach
+//     // All the variables are in the top of the JS
+//   });
+// };
+
+// displayMovments(account1.movements);
+
+// ==================================================================
+// =================== CODING CHALLENGE #1 ==========================
+// ==================================================================
+
+/* 
+Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
+
+Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
+
+1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats, not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
+2. Create an array with both Julia's (corrected) and Kate's data
+3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 🐶")
+4. Run the function for both test datasets
+
+HINT: Use tools from all lectures in this section so far 😉
+
+TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
+const checkDogs = function (dogsJulia, dogsKate) {
+  const copy = dogsJulia.slice(1, 4);
+  copy.concat(dogsKate).forEach(function (age, index, array) {
+    age > 5
+      ? console.log(
+          `Dog number ${index + 1} is an adult, and is ${age} years old`
+        )
+      : console.log(`Dog number ${index + 1} is still a puppy 🐶`);
   });
+  console.log(dogsJulia, copy);
 };
-
-displayMovments(account1.movements);
+checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
