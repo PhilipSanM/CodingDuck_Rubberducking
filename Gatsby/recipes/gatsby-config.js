ad6299c106480c8bcdb1d211a1b5733af55cf583
PhilipSanM <felipesanchezmart@gmail.com>
2022-08-05 11:16:15 -0500

    Source file system plugin

--- Gatsby/recipes/gatsby-config.js
+++ Gatsby/recipes/gatsby-config.js
@@ -6,10 +6,28 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: "Simply Recipes",
+    description: "Nice and clean recipes stie",
+    author: "@Me",
+    person: { name: "pipe", age: 19 },
+    simpleData: ["item 1", "item 2"],
+    complexData: [
+      { name: "pipe", age: 19 },
+      { name: "Jonh", age: 22 },
+    ],
+  },
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
   ],
 }
