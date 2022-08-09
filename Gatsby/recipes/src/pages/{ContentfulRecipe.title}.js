0b4536d47d35aac198bc6641f15fa5b2783db1b3
Felipe Sanchez Martinez <felipesanchezmart@gmail.com>
2022-08-09 11:06:21 -0500

    Recipes Page v_1.1

--- Gatsby/recipes/src/pages/{ContentfulRecipe.title}.js
+++ Gatsby/recipes/src/pages/{ContentfulRecipe.title}.js
@@ -1,7 +1,35 @@
 import React from "react"
-
-const RecipeTemplate = () => {
-  return <div>RecipeTemplate</div>
+import { graphql } from "gatsby"
+const RecipeTemplate = props => {
+  console.log(props)
+  return (
+    <div>
+      <h2>{props.params.title}</h2>
+    </div>
+  )
 }
+
+export const query = graphql`
+  query getSingleRecipe($title: String) {
+    contentfulRecipe(title: { eq: $title }) {
+      cookTime
+      title
+      content {
+        ingredients
+        instructions
+        tags
+        tools
+      }
+      description {
+        description
+      }
+      prepTime
+      servings
+      image {
+        gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG)
+      }
+    }
+  }
+`
 
 export default RecipeTemplate
