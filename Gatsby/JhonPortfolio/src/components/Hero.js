c2f70e50573f31994f3900bac06a1af252665b95
Felipe Sanchez Martinez <felipesanchezmart@gmail.com>
2022-08-10 11:42:19 -0500

    New files for JhonPortfolio

--- Gatsby/JhonPortfolio/src/components/Hero.js
+++ Gatsby/JhonPortfolio/src/components/Hero.js
@@ -3,9 +3,39 @@ import { Link } from "gatsby"
 import socialLinks from "../constants/social_links"
 // import heroImg from "../assets/images/hero.svg"
 import { StaticImage } from "gatsby-plugin-image"
-
+//from undraw
 const Hero = () => {
-  return <h2>hero component</h2>
+  return (
+    <header className="hero">
+      <section className="section-center hero-center">
+        <article className="hero-info">
+          <div>
+            <div className="underline"> </div>
+            <h1>I'm felipe</h1>
+            <h4>AI student</h4>
+            <Link to="/contact" className="btn">
+              Contact me
+            </Link>
+            <div className="social-links">
+              {socialLinks.map(link => {
+                return (
+                  <a href={link.url} key={link.id} className="social-link">
+                    {link.icon}
+                  </a>
+                )
+              })}
+            </div>
+          </div>
+        </article>
+        <StaticImage
+          src="../assets/images/hero.svg"
+          alt="portfolio"
+          className="hero-img"
+          placeholder="blurred"
+        />
+      </section>
+    </header>
+  )
 }
 
 export default Hero
