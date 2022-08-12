84c55d34e1b1645c5dcfce3b501de26f3478f22f
Felipe Sanchez Martinez <felipesanchezmart@gmail.com>
2022-08-11 20:01:48 -0500

    Init of strapi in my portfolio

--- Gatsby/philip/gatsby-config.js
+++ Gatsby/philip/gatsby-config.js
@@ -3,8 +3,34 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 
+const strapiConfig = {
+  apiURL: process.env.STRAPI_API_URL,
+  accessToken: process.env.STRAPI_TOKEN,
+  collectionTypes: ["job", "project"],
+  singleTypes: [],
+}
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+
+    {
+      resolve: `gatsby-source-strapi`,
+      options: strapiConfig,
+    },
+  ],
 }
