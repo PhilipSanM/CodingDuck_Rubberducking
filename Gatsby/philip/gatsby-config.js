89c0b51971e41317a776ccc7c580c1908fcdc2aa
Felipe Sanchez Martinez <felipesanchezmart@gmail.com>
2022-08-13 12:50:21 -0500

    Main template finished

--- Gatsby/philip/gatsby-config.js
+++ Gatsby/philip/gatsby-config.js
@@ -11,7 +11,7 @@ const strapiConfig = {
   apiURL: process.env.STRAPI_API_URL,
   accessToken: process.env.STRAPI_TOKEN,
   collectionTypes: ["job", "project"],
-  singleTypes: [],
+  singleTypes: [`about`],
 }
 module.exports = {
   /* Your site config here */
