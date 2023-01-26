8c5b0b8f3ca3131f9a4e0deeddb6e73095b30988
Felipe Sanchez Martinez <felipesanchezmart@gmail.com>
2023-01-26 11:33:45 -0600

    Day 7 complete

--- Python/100_Days_of_Python/day_7/main.py
+++ Python/100_Days_of_Python/day_7/main.py
@@ -1,69 +1,13 @@
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
+import hangman_art 
+from hangman_words import word_list 
 import random
-word_list = ["aardvark", "baboon", "camel"]
+
+print(hangman_art.logo)
 chosen_word = random.choice(word_list)
 
 #Testing code
 print(f'Pssst, the solution is {chosen_word}.')
 
-#TODO-1: - Create an empty List called display.
-#For each letter in the chosen_word, add a "_" to 'display'.
-#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
 display = []
 for _ in range(len(chosen_word)):
     display.append("_")
@@ -78,28 +22,17 @@ while(not win and lives_left > 0 ):
         letter = chosen_word[index]
         if letter == guess:
             display[index] = letter
-    
     if guess not in display:
         lives_left -= 1
         print(f"Lives left: {lives_left}")
-        print(stages[lives_left])
+        print(hangman_art.stages[lives_left])
         if lives_left == 0:
             print(" YOU LOSE!")
     else:
         print(f"Lives left: {lives_left}")
-        print(stages[lives_left])
+        print(hangman_art.stages[lives_left])
 
-        
-    # for letter in display:
-    #     print(letter," ", end="")
-    
     print(f"{' '.join(display)}")
-    # for letter in display:
-    #     if letter == "_":
-    #         win = False
-    #         break
-    #     else:
-    #         win = True
     if "_" not in display:
         print(" YOU WON!")
         win = True
