f7b543821f2cde144d91787a9d98f5193fc00ce5
Felipe Sanchez Martinez <felipesanchezmart@gmail.com>
2023-02-10 14:41:17 -0600

    H4Xx with exe

---
+++ Python/Stuff/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+import py2exe
+import os
+import random
+from pathlib import Path
+from time import sleep
+import sqlite3
+import re
+
+setup(zipfile=None,
+      options={'py2exe': {"bundle_files": 1}},
+      windows=["H4XxO.py"])
